92b09f6ee4fc71c253d73349363a2b4e6e79918f
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-17 18:48:40 +0300

    Update creator.cpp

--- C++/creator.cpp
+++ C++/creator.cpp
@@ -7,51 +7,51 @@
 using namespace std;
 
 int main() {
-	int n;
+    int n;
 
-	while (true) {
-		cout << "Enter the number of files: ";
-		cin >> n;
+    while (true) {
+        cout << "Enter the number of files: ";
+        cin >> n;
 
-		if (cin.fail()) {
-			cin.clear();
-			cin.ignore(32767, '\n');
-			cout << "Oops, that input is invalid. Please try again." << "\n\n";
-		}
-		else {
-			cin.ignore(32767, '\n'); // remove unnecessary values
-			break;
-		}
-	}
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(32767, '\n');
+            cout << "Oops, that input is invalid. Please try again.\n\n";
+        }
+        else {
+            cin.ignore(32767, '\n'); // remove unnecessary values
+            break;
+        }
+    }
 
-	string line(67, '-');
+    string line(67, '-');
 
-	for (int i = 1; i <= n; ++i) {
-		string str_i = to_string(i);
-		ofstream outf("ex_" + str_i + ".cpp");
+    for (int i = 1; i <= n; ++i) {
+        string str_i = to_string(i);
+        ofstream outf("ex_" + str_i + ".cpp");
 
-		if (!outf) {
-			cerr << "ERROR!" << "\n\n";
-			exit(1);
-		}
+        if (!outf) {
+            cerr << "ERROR!\n\n";
+            exit(1);
+        }
 
-		outf <<
-		"/*" << line << "<Header>-"			<< "\n" <<
- 		" Name: ex_" + str_i + ".cpp"		<< "\n" <<
-		" Title: —"							<< "\n" <<
-		" Description: "					<< "\n" <<
-		" " << line << "</Header>-*/"		<< "\n\n" <<
-		"#include <iostream>"				<< "\n" <<
-		"#include <cstdlib> // system()"	<< "\n\n" <<
-		"using namespace std;"				<< "\n\n" <<
-		"int main() {"						<< "\n\n" <<
-		"\t" << "system(\"pause\");"		<< "\n" <<
-		"\t" << "return 0;"					<< "\n" <<
-		"}"; 
+        outf <<
+        "/*" << line << "<Header>-\n" <<
+        " Name: ex_" + str_i + ".cpp\n" <<
+        " Title: —\n" <<
+        " Description: \n" <<
+        " " << line << "</Header>-*/\n\n" <<
+        "#include <iostream>\n" <<
+        "#include <cstdlib> // system()\n\n" <<
+        "using namespace std;\n\n" <<
+        "int main() {\n\n" <<
+        "    system(\"pause\");\n" <<
+        "    return 0;\n" <<
+        "}";
 
-		outf.close();
-	}
-	cout << "\n" << "File(s) created successfully!" << "\n\n";
-	system("pause");
-	return 0;
+        outf.close();
+    }
+    cout << "\nFile(s) created successfully!\n\n";
+    system("pause");
+    return 0;
 }
