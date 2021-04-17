95d09dfd0bb2c2df6bb8fc646480163d3aed5d2e
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-17 17:47:54 +0300

    Create creator.cpp

---
+++ C++/creator.cpp
@@ -0,0 +1,57 @@
+// Create cpp-files in the current folder
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib> // system()
+
+using namespace std;
+
+int main() {
+	int n;
+
+	while (true) {
+		cout << "Enter the number of files: ";
+		cin >> n;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(32767, '\n');
+			cout << "Oops, that input is invalid. Please try again." << "\n\n";
+		}
+		else {
+			cin.ignore(32767, '\n'); // remove unnecessary values
+			break;
+		}
+	}
+
+	string line(67, '-');
+
+	for (int i = 1; i <= n; ++i) {
+		string str_i = to_string(i);
+		ofstream outf("ex_" + str_i + ".cpp");
+
+		if (!outf) {
+			cerr << "ERROR!" << "\n\n";
+			exit(1);
+		}
+
+		outf <<
+		"/*" << line << "<Header>-"			<< "\n" <<
+ 		" Name: ex_" + str_i + ".cpp"		<< "\n" <<
+		" Title: —"							<< "\n" <<
+		" Description: "					<< "\n" <<
+		" " << line << "</Header>-*/"		<< "\n\n" <<
+		"#include <iostream>"				<< "\n" <<
+		"#include <cstdlib> // system()"	<< "\n\n" <<
+		"using namespace std;"				<< "\n\n" <<
+		"int main() {"						<< "\n\n" <<
+		"\t" << "system(\"pause\");"		<< "\n" <<
+		"\t" << "return 0;"					<< "\n" <<
+		"}"; 
+
+		outf.close();
+	}
+	cout << "\n" << "File(s) created successfully!" << "\n\n";
+	system("pause");
+	return 0;
+}
