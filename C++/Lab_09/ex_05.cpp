3b2a36c4b25b3a114ebe0cd0f0b8d5074a5f7eef
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-19 18:31:47 +0300

    _

---
+++ C++/Lab_09/ex_05.cpp
@@ -0,0 +1,52 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_05.cpp
+ Title: —
+ Description: Создайте свой манипулятор вывода sethex(), который осуществляет
+ вывод в шестнадцатиричной системе счисления и устанавливает флаги uppercase
+ и showbase. Кроме того, создайте манипулятор вывода reset(), который отменяет
+ изменения, сделанные манипулятором sethex().
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+void showflags();
+ostream& sethex(ostream &stream);
+ostream& reset(ostream &stream);
+
+int main() {
+    // showflags();
+    cout << sethex << 100 << "\n\n";
+    // showflags();
+    cout << reset << 100 << "\n\n";
+    // showflags();
+
+    system("pause");
+    return 0;
+}
+
+ostream& sethex(ostream &stream) {
+    stream.setf(ios::hex, ios::basefield);
+    stream.setf(ios::uppercase | ios::showbase);
+    return stream;
+}
+
+ostream& reset(ostream &stream) {
+    stream.unsetf(ios::hex | ios::uppercase | ios::showbase);
+    stream.setf(ios::dec);
+    return stream;
+}
+
+void showflags() {
+    ios::fmtflags f;
+    f = cout.flags(); // get flag
+    if (f & ios::dec) cout << "dec on\n";
+    else cout << "dec off\n";
+    if (f & ios::oct) cout << "oct on\n";
+    else cout << "oct off\n";
+    if (f & ios::hex ) cout << "hex on\n";
+    else cout << "hex off\n";
+    cout << "\n";
+}
---
+++ C++/Lab_09/ex_06.cpp
@@ -0,0 +1,44 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_06.cpp
+ Title: —
+ Description: Создайте манипулятор ввода skipchar(), который поочередно то
+ считывает, то пропускает каждые 5 символов потока ввода.
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+using namespace std;
+
+vector<char> v;
+
+istream& skipchar(istream &in) {
+    bool run = true;
+    char ch;
+    in.unsetf(ios::skipws);
+    while (run) {
+        for (int i = 0; i < 5 && run; ++i) {
+            in >> ch;
+            if (ch == '\n') { run = false; }
+            v.push_back(ch);
+        }
+        for (int i = 0; i < 5 && run; ++i) {
+            in >> ch;
+            if (ch == '\n') { run = false; }
+        }
+    }
+    return in;
+}
+
+int main() {
+    cout << " Enter the text: ";
+    cin >> skipchar;
+    for (size_t i = 0; i < v.size(); ++i) {
+        cout << v[i];
+    }
+    cout << "\n\n";
+
+    system("pause");
+    return 0;
+}
