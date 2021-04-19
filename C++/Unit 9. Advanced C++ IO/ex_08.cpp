facfa4d44bae8075defc0a1732b50ab841d771a6
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-19 21:16:14 +0300

    _

---
+++ C++/Unit 9. Advanced C++ IO/ex_08.cpp
@@ -0,0 +1,33 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_08.cpp
+ Title: —
+ Description: Напишите программу для записи в файл phones.txt данных типа:
+ Ivan Tormozenko, +38067 415 5113
+ Petro Siplusenko, +38068 212 5332
+ Olena Kompailenko, +38050 202 5451
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    ofstream fout("phones.txt");
+    if (!fout) {
+        cerr << " phones.txt cannot be opened for writing!" << endl;
+        exit(1);
+    }
+    fout << "Ivan Tormozenko, +38067 415 5113" << endl;
+    fout << "Petro Siplusenko, +38068 212 5332" << endl;
+    fout << "Olena Kompailenko, +38050 202 5451" << endl;
+    cout << "\nSuccessfully written to file\n\n";
+    
+    // fout.close();
+    // When outf goes out of scope, the destructor of the ofstream class
+    // will automatically close our file.
+
+    system("pause");
+    return 0;
+}
