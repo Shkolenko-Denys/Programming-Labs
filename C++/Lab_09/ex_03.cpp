9d16c03f7e7a88618bd943904b0f8d1fa6dd0618
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-19 17:03:34 +0300

    _

---
+++ C++/Lab_09/ex_03.cpp
@@ -0,0 +1,61 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_03.cpp
+ Title: —
+ Description: Дан следующий класс. Создайте для него пользовательские функции
+ ввода и вывода.
+ class date {
+     char d[9]; // store date as string : mm/dd/yy
+ public:
+     // add inserter and extractor
+ };
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class Date {
+    char d[9]; // store date as string : mm/dd/yy
+public:
+    Date() { d[9] = {'\0'}; }
+    friend ostream& operator<< (ostream &out, Date &obj);
+    friend istream& operator>> (istream &in, Date &obj);
+};
+
+ostream& operator<< (ostream &out, Date &obj) {
+    out << obj.d;
+    return out;
+}
+
+istream& operator>> (istream &in, Date &obj) {
+    cout << " Month: ";
+    int month;
+    in >> month;
+    obj.d[0] = '0' + (month / 10);
+    obj.d[1] = '0' + (month % 10);
+
+    cout << " Day: ";
+    int day;
+    in >> day;
+    obj.d[3] = '0' + (day / 10);
+    obj.d[4] = '0' + (day % 10);
+
+    cout << " Year: ";
+    int year;
+    in >> year;
+    obj.d[6] = '0' + (year - 2000) / 10;
+    obj.d[7] = '0' + (year - 2000) % 10;
+
+    obj.d[2] = obj.d[5] = '/';
+    return in;
+}
+
+int main() {
+    Date date;
+    cin >> date;
+    cout << date << "\n\n";
+
+    system("pause");
+    return 0;
+}
---
+++ C++/Lab_09/ex_04.cpp
@@ -0,0 +1,28 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_04.cpp
+ Title: —
+ Description: Создайте свой манипулятор вывода для отображения текущего
+ системного времени и даты. Назовите этот манипулятор tdm().
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+ostream& tdm(ostream &out) {
+	time_t rawtime;
+	struct tm *timeinfo;
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+	out << "The current date/time is: " << asctime(timeinfo);
+	return out;
+}
+
+int main() {
+	cout << "\n" << tdm << "\n\n";
+
+	system("pause");
+	return 0;
+}
