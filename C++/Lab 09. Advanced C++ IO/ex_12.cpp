6d0955c05b665c2578069cacf7764c48a900e539
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-20 17:28:31 +0300

    _

---
+++ C++/Lab 09. Advanced C++ IO/ex_12.cpp
@@ -0,0 +1,28 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_12.cpp
+ Title: —
+ Description: Перепишите программу из Example 10.11 так, чтобы вместо функции
+ getline() использовать функцию get(). Будет ли отличаться работа программы?
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+    char str[80];
+    cout << " Input : ";
+    cin.get(str, 80);
+    cout << " Output: " << str << "\n\n";
+    
+    // the program only reads the first 79 characters (+ null terminator).
+    // The rest of the characters remain in the input stream.
+    // One important caveat: the get() function does not read the newline character!
+
+    // the getline() function works exactly the same as the get() function,
+    // but it can read newline characters.
+
+    system("pause");
+    return 0;
+}
