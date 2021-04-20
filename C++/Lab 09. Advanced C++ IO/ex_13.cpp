d67c487c57a9cdf5d973db4123031f67112c8693
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-04-20 17:47:55 +0300

    _

---
+++ C++/Lab 09. Advanced C++ IO/ex_13.cpp
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_13.cpp
+ Title: —
+ Description: Напишите программу для построчного считывания текстового файла и
+ вывода каждой считанной строки на экран. Используйте функцию getline().
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "\n Error! Call: ex_13.exe text_doc_ex_13.txt" << endl;
+    } else {
+        ifstream fin(argv[1]);
+        if (!fin) {
+            cerr << " text_doc_ex_13.txt could not be opened for reading!" << endl;
+            exit(1);
+        }
+
+        string strBuf;
+        while (fin) {
+            getline(fin, strBuf);
+            if (fin) {
+                cout << strBuf << endl;
+            }
+            fin.close();
+        }
+    }
+    system("pause");
+    return 0;
+}
