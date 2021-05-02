470d8e74d757937541ddf95891922a53d8c1d52d
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-05-02 20:30:55 +0300

    _

---
+++ C++/Lab 11. Templates and Exception Handling/ex_01.cpp
@@ -0,0 +1,30 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_01.cpp
+ Title: —
+ Description: Напишите родовую функцию min(), возвращающую меньший из двух
+ своих аргументов. Например, версия функции min(3, 4) должна возвратить 3,
+ а версия min('c', 'a') - а. Продемонстрируйте работу функции с помощью
+ программы.
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cstdlib>
+
+template <typename T>
+const T& min(const T &a, const T &b) {
+    return (a < b) ? a : b;
+}
+
+int main() {
+    int i = min(3, 4);
+    std::cout << i << "\n";
+ 
+    double d = min(3.33, 4.44);
+    std::cout << d << "\n";
+ 
+    char ch = min('c', 'a');
+    std::cout << ch << "\n\n";
+
+    system("pause");
+    return 0;
+}
---
+++ C++/Lab 11. Templates and Exception Handling/ex_02.cpp
@@ -0,0 +1,54 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_02.cpp
+ Title: —
+ Description: Функция find() ищет объект в массиве. Она возвращает либо индекс
+ найденного объекта (если его удалось найти), либо -1, если заданный объект
+ не найден. Ниже представлен прототип конкретной версии функции find():
+ int find(int object, int *list, int size) {
+    // Параметр size задает количество элементов массива.
+ }
+ Переделайте функцию find() в родовую функцию и продемонстрируйте ваше решение
+ в программе.
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cmath>
+#include <limits>
+#include <cstdlib>
+
+template <typename T>
+size_t find(const T &object, const T *const list, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        if (list[i] == object) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// Two floating point numbers cannot be compared
+bool is_equal(double x, double y) {
+    return std::fabs(x - y) < std::numeric_limits<double>::epsilon();
+}
+template <>
+size_t find(const double &object, const double *const list, size_t size) {
+    for (size_t i = 0; i < size; ++i) {
+        if (is_equal(list[i], object)) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    int int_arr[5] = {0, 1, 2, 3, 4};
+    double double_arr[5] = {0.0, 1.1, 2.2, 3.3, 4.4};
+    char char_arr[5] = {'a', 'b', 'c', 'd', 'e'};
+
+    std::cout << " elem 2 by index " << find(2, int_arr, 5) << "\n";
+    std::cout << " elem 3.3 by index " << find(3.3, double_arr, 5) << "\n";
+    std::cout << " elem e by index " << find('e', char_arr, 5) << "\n\n";
+
+    system("pause");
+    return 0;
+}
