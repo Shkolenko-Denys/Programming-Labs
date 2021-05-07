78958b543286ad90b49fd42853d5543bc54c4bac
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-05-08 00:13:40 +0300

    _

---
+++ C++/Lab 11. Templates and Exception Handling/ex_12.cpp
@@ -0,0 +1,58 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_12.cpp
+ Title: operator new example
+ Description: Вопрос для размышления: в чем, по вашему мнению, при неудачной
+ попытке выделения памяти преимущество возбуждения исключительной ситуации
+ оператором new по сравнению с возвращением нуля?
+ -------------------------------------------------------------------</Header>-*/
+
+// Розглянемо приклад, де відбувається ДВА динамічних виділення пам'яті:
+// зовнішнє (main) і всередині класу MyClass
+
+#include <iostream>     // std::cout
+#include <cstdlib>      // system
+
+class MyClass {
+    int *data;
+public:
+    MyClass() {
+        std::cout << "constructed [" << this << "]\n";
+        data = new (std::nothrow) int[100000]();
+        if (!data) {
+            // Нехай тут відбудеться збій при виділенні пам'яті для data.
+            // Без генерации исключительной ситуации сдесь не обойтись.
+            // Исключения позволяют обрабатывать вложенные сбои так же, как и
+            // внешние (раскручивание стека).
+            // Это мощный механизм избежания дублирования кода и ошибок.
+            std::cerr << " Allocation Error\n";
+            exit(EXIT_FAILURE);
+        }
+    }
+    ~MyClass() {
+        delete[] data;
+    }
+};
+
+int main () {
+    std::cout << " 1: ";
+    MyClass *p1 = new (std::nothrow) MyClass;
+    if (!p1) {
+        // ...
+    }
+
+    std::cout << " 2: ";
+    MyClass *p2;
+    try {
+        p2 = new MyClass;
+    }
+    catch (std::bad_alloc &ba) {
+        std::cerr << " bad_alloc caught: " << ba.what() << '\n';
+    }
+
+    // ...
+
+    delete p1;
+    delete p2;
+    system("pause");
+    return 0;
+}
---
+++ C++/Lab 11. Templates and Exception Handling/ex_14.cpp
@@ -0,0 +1,87 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_14.cpp
+ Title: —
+ Description: В Unit5 "Bounded Array" рассматривался простейший способ
+ реализации безопасного массива, в котором для доступа к элементам массива
+ использовались функции get() и put(). Перегрузка оператора [] позволяет
+ создать такой массив гораздо проще. В практическом задании для Unit7 вам
+ поручалось для создания безопасного массива реализовать в функции
+ operator[]() контроль границ. Кроме этого, функция operator[]() должна
+ возвращать ссылку на индексируемый элемент. В представленном ниже фрагменте
+ сделана попытка добавить контроль границ массива, что позволяет при нарушении
+ границ генерировать соответствующую ошибку.
+ Подсказка: Вспомните, что безопасный массив - это массив, который
+ инкапсулирован в классе, и при этом класс обеспечивает контроль границ
+ массива. Благодаря перегрузке оператора [], работать с безопасным массивом
+ можно так же, как с обычным.
+ // A safe array example:
+ ...
+ class arraytype {
+    ...
+ };
+ // Provide range checking for arraytype .
+ int &arraytype::operator [](int i) {
+    if (...) {
+    cout << "Index value of " << i << " is out of bounds.\n";
+    ...
+ }
+ ...
+ }
+ int main() {
+    arraytype obj;
+    // this generates a run - time error because size+100 is out of range:
+    obj[size+100] = 99; // error
+    return 0;
+    }
+ Итак, у вас есть уже класс с безопасным массивом. Переделайте его в родовой
+ класс с безопасным массивом.
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+template <class T, int size = 10>
+class arraytype {
+    T arr[size];
+public:
+    arraytype() {
+        for (int i = 0; i < size; i++) {
+            arr[i] = (T) i / 2;
+        }
+    }
+    T& operator[] (const int&);
+};
+
+template <class T, int size>
+T& arraytype<T, size>::operator[](const int &i) {
+    if (i < 0 || i >= size) {
+        throw i;
+    }
+    return arr[i];
+}
+
+int main() {
+    arraytype<int> int_obj;
+    arraytype<double> double_obj;
+    cout << " Int array: ";
+    for (int i = 0; i < 10; i++) {
+        cout << int_obj[i] << " ";
+    }
+    cout << "\n";
+
+    cout << " Double array: ";
+    for (int i = 0; i < 10; i++) {
+        cout << double_obj[i] << " ";
+    }
+    cout << "\n";
+
+    try {
+        int_obj[12] = 100;
+    } catch (int i) {
+        cout << "Index value of " << i << " is out of bounds.\n";
+    }
+    system("pause");
+    return 0;
+}
