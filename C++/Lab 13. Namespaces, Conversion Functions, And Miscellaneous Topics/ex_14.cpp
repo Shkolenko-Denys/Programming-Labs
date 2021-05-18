79f64765bc7ac79c5337488cdf05aa45131772bc
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-05-18 19:40:14 +0300

    _

---
+++ C++/Lab 13. Namespaces, Conversion Functions, And Miscellaneous Topics/ex_14.cpp
@@ -0,0 +1,43 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_14.cpp
+ Title: —
+ Description: Вопрос для размышления: вернитесь к примерам первых 13-ти лекций.
+ Подумайте о том, в каких из них функции-члены можно было бы сделать
+ постоянными или статическими. Может быть это примеры, в которых определение
+ пространств имен наиболее предпочтительно? Пары примеров будет достаточно.
+ -------------------------------------------------------------------</Header>-*/
+
+// Example #1
+class coord {
+    int x, y; // coordinate values
+public:
+    coord() { x = 0; y = 0; }
+    coord(int i, int j) { x = i; y = j; }
+
+    int get_x() const { return x; } // used const
+    int get_y() const { return y; } // used const
+    void set_x(int i) { x = i; }
+    void set_y(int j) { y = j; }
+};
+
+
+// Example #2
+class Num {
+protected:
+    int n;
+public:
+    Num() { n = 0; }
+    Num(const int &num) { n = num; }
+    virtual void shownum() const { // used const
+        cout << n << endl;
+    }
+};
+
+class Outhex : public Num {
+public:
+    Outhex() { n = 0; }
+    Outhex(const int &num) { n = num; }
+    virtual void shownum() const override { // used const
+        cout << hex << n << endl;
+    }
+};
