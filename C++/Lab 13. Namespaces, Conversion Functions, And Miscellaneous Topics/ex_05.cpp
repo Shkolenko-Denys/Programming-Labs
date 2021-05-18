7d12a1f7d41903fafed48e6619b2e345f0893995
Shkolenko-Denys <shkolenko2003@gmail.com>
2021-05-18 15:50:37 +0300

    _

---
+++ C++/Lab 13. Namespaces, Conversion Functions, And Miscellaneous Topics/ex_05.cpp
@@ -0,0 +1,41 @@
+/*-------------------------------------------------------------------<Header>-
+ Name: ex_05.cpp
+ Title: —
+ Description: Одним из интересных применений статических переменных-членов
+ является хранение информации о количестве объектов класса, существующих в
+ каждый конкретный момент времени. Для этого необходимо увеличивать на единицу
+ статическую переменнуючлен каждый раз, когда вызывается конструктор класса,
+ и уменьшать на единицу, когда вызывается деструктор. Реализуйте эту схему и
+ продемонстрируйте ее работу.
+ -------------------------------------------------------------------</Header>-*/
+
+#include <iostream>
+
+using namespace std;
+
+class Example {
+    static int count;
+    const int data;
+public:
+    Example(const int &d) : data(d) {
+        count++;
+    }
+    ~Example() {
+        count--;
+    }
+    int get_count() { return count; }
+};
+
+int Example::count = 0;
+
+int main() {
+    Example obj1(12), obj2(2);
+    cout << "Count " << obj1.get_count() << "\n";
+
+    Example *new_obj = new Example(13);
+    cout << "Count " << obj1.get_count() << "\n";
+    
+    delete new_obj;
+    cout << "Count " << obj1.get_count() << "\n";
+    return 0;
+}
