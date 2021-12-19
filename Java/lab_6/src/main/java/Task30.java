df305f1cd667762421789d158151e508ce53aca9
Shkolenko Denys <69078357+Shkolenko-Denys@users.noreply.github.com>
2021-12-19 23:45:04 +0200

    task 30

--- Java/lab_6/src/main/java/Task30.java
+++ Java/lab_6/src/main/java/Task30.java
@@ -5,7 +5,7 @@ import java.util.Objects;
 
 /**
  * Изменение размера изображения в графическом окне. В верхней панели окна
- * (JPanel) "Размер изображения"  заданы следующие компоненты: надпись (JLabel)
+ * (JPanel) "Размер изображения" заданы следующие компоненты: надпись (JLabel)
  * "Ширина:", бегунок (JSlider), надпись (JLabel) "Высота:", бегунок
  * (JSlider) и кнопка (JButton): "Вывести изображение". В нижней панели окна
  * (JPanel) окна "Вывод изображения" в компоненте (JLabel) задается произвольное
@@ -21,9 +21,47 @@ public class Task30 extends JFrame {
         JFrame.setDefaultLookAndFeelDecorated(true);
         JFrame frame = new JFrame("Task 30");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel jPanelControl = new JPanel();
+        jPanelControl.setLayout(new GridLayout(4,2));
+        TitledBorder borderControl = BorderFactory.createTitledBorder("Размер изображения");
+        jPanelControl.setBorder(borderControl);
+
+        jPanelControl.add(new JLabel("Ширина:"));
+        BoundedRangeModel modelWidth = new DefaultBoundedRangeModel(80, 0, 10, 150);
+        JSlider sliderWidth = new JSlider(modelWidth);
+        sliderWidth.setPaintLabels(true);
+        sliderWidth.setMajorTickSpacing(10);
+        jPanelControl.add(sliderWidth);
+
+        jPanelControl.add(new JLabel("Высота:"));
+        BoundedRangeModel modelHeight = new DefaultBoundedRangeModel(80, 0, 10, 150);
+        JSlider sliderHeight = new JSlider(modelHeight);
+        sliderHeight.setPaintLabels(true);
+        sliderHeight.setMajorTickSpacing(10);
+        jPanelControl.add(sliderHeight);
+
+        JPanel jPanelButton = new JPanel();
+        JButton enterButton = new JButton("Вывести изображение");
+        jPanelButton.add(enterButton);
+
+        JPanel jPanelImage = new JPanel();
+        ImageIcon icon = new ImageIcon("");
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(icon);
+        jPanelImage.add(enterButton);
+
+        frame.getContentPane().add(jPanelControl, BorderLayout.NORTH);
+        frame.getContentPane().add(jPanelButton, BorderLayout.SOUTH);
+        frame.getContentPane().add(jPanelImage);
+
+        frame.setPreferredSize(new Dimension(500, 500));
+        frame.setLocation(500, 100);
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(Task16::new);
+        SwingUtilities.invokeLater(Task30::new);
     }
 }
